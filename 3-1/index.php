efdfa8e1b047ad79d6db5f0cf2228816770c237b
higuka <elephantyouyou@yahoo.co.jp>
2022-12-28 01:40:51 +0900

    FizzBuzz問題
    FizzBuzz問題

---
+++ 3-1/index.php
@@ -0,0 +1,24 @@
+<?php
+// FizzBuzz問題
+$i = 1;
+for ($i = 1; $i<=100; $i++){
+
+if ($i % 15 === 0){
+  echo 'FizzBuzz!!';
+}
+
+elseif ($i % 3 === 0) {
+  echo 'Fizz!';
+}
+
+elseif ($i % 5 === 0) {
+  echo 'Buzz!';
+}
+
+else {
+  echo $i;
+
+}
+echo '<br>';
+}
+?>
