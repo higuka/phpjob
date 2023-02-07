13358b09fe0ead044faec9273810bd3f0a9cf5f5
higuka <elephantyouyou@yahoo.co.jp>
2023-02-08 01:27:58 +0900

    ３章練習問題1修正(2/8)
    確認よろしくお願いいたします。(2/8)

--- 3-2/index.php
+++ 3-2/index.php
@@ -1,34 +1,42 @@
 <!-- step1:フルーツと単価の連想配列を作成してください。 -->
 <?php
 
-$fruits = ["apple" => "リンゴ", "orange" => "みかん", "peach" => "桃"];
-$fruitPrise = ["apple" => "300", "orange" => "150", "peach" => "3000"];
+$fruits = ["りんご" => 300, "みかん" => 150, "桃" => 3000];
 
-echo "{$fruits["apple"]}は{$fruitPrise["apple"]}円です。"."<br />";
-echo "{$fruits["orange"]}は{$fruitPrise["orange"]}円です。"."<br />";
-echo "{$fruits["peach"]}は{$fruitPrise["peach"]}円です。"."<br />";
-
+$fruits_array = ['りんご' => 0, 'みかん' => 1, '桃' => 2];
+var_dump($fruits_array);
+echo '<br>';
 ?>
 
 <!-- step2:単価を計算する関数を定義してください。 -->
+<!-- ※リンゴ2個の合計を求めました -->
+
+<?php
+
+$price1 = 300;
+
+function fruits_sale($price1 , $kosu = 2){
+$sale = $price1 * $kosu ; 
+return $sale. "円";
+
+}
+
+echo fruits_sale($price1);
+echo '<br>';
+?>
+
 <!-- step3:繰り返しを使ってそれぞれのフルーツを書き出してください。 -->
 <!-- ※果物2個の合計を求めました -->
 
 <?php
+$i = 0;
+$fruits = ["りんご" => 300, "みかん" => 150, "桃" => 3000];
 
-$fruit_name = ["リンゴ", "みかん", "桃"];
-
-$price1 = 300;
-$price2 = 150;
-$price3 = 3000;
-
-function furuits_sale($price){
-  $sale = $price * 2 ; 
-    return $fruit_name."2個の値段は".$sale."円です｡";
+foreach ($fruits as $key => $value) {
+    echo $key."は".fruits_sale($value ,  $kosu = 2)."円です。";
+     $i++;
+     echo '<br>';
+   
 }
-
-echo furuits_sale($price1);
-echo furuits_sale($price2);
-echo furuits_sale($price3);
 
 ?>
