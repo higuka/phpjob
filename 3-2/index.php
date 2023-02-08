277d34f788837e6d6334fc56299d4d66159e7802
higuka <elephantyouyou@yahoo.co.jp>
2023-02-09 01:30:32 +0900

    ３章練習問題１(2/9)
    再度修正しました｡確認よろしくお願いいたします。

--- 3-2/index.php
+++ 3-2/index.php
@@ -3,40 +3,34 @@
 
 $fruits = ["りんご" => 300, "みかん" => 150, "桃" => 3000];
 
-$fruits_array = ['りんご' => 0, 'みかん' => 1, '桃' => 2];
+$fruits_array = [1, 1, 1];
 var_dump($fruits_array);
 echo '<br>';
 ?>
 
 <!-- step2:単価を計算する関数を定義してください。 -->
-<!-- ※リンゴ2個の合計を求めました -->
+
 
 <?php
+$i = 0;
 
-$price1 = 300;
-
-function fruits_sale($price1 , $kosu = 2){
-$sale = $price1 * $kosu ; 
-return $sale. "円";
-
+function fruits_sale($fruits , $fruits_array){
+$sale = $fruits * $fruits_array ; 
+return $sale ;
 }
+ 
+echo fruits_sale();
 
-echo fruits_sale($price1);
 echo '<br>';
 ?>
 
 <!-- step3:繰り返しを使ってそれぞれのフルーツを書き出してください。 -->
-<!-- ※果物2個の合計を求めました -->
 
 <?php
-$i = 0;
-$fruits = ["りんご" => 300, "みかん" => 150, "桃" => 3000];
 
 foreach ($fruits as $key => $value) {
-    echo $key."は".fruits_sale($value ,  $kosu = 2)."円です。";
-     $i++;
-     echo '<br>';
-   
-}
-
+    echo $key."は".fruits_sale($value , $fruits_array[$i])."円です。";
+    $i++;    
+   }
+echo '<br>';
 ?>
