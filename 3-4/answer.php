686567c55158891d9dd37f04796aa79bcd7c27f7
higuka <elephantyouyou@yahoo.co.jp>
2023-02-28 23:13:16 +0900

    ３章チェックテスト(2/28修正)
    よろしくお願いいたします。

--- 3-4/answer.php
+++ 3-4/answer.php
@@ -3,9 +3,14 @@
 //[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
 $my_name = $_POST['my_name'];
 
+$test1 = $_POST['test1'];
+$test2 = $_POST['test2'];
+$test3 = $_POST['test3'];
+
 $ans_test1 = $_POST['ans_test1'];
 $ans_test2 = $_POST['ans_test2'];
 $ans_test3 = $_POST['ans_test3'];
+
 
 //選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
 
