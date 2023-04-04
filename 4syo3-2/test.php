b8d9d8f8b772842a3e0ad2a73effaa108b12fe56
higuka <elephantyouyou@yahoo.co.jp>
2023-04-04 16:06:17 +0900

    4章PDO実装
    よろしくお願いいたします。

---
+++ 4syo3-2/test.php
@@ -0,0 +1,19 @@
+<?php
+// 作成したdb_connect.phpを読み込む
+require_once('db_connect.php');
+
+// 実行したいSQL文を準備
+// 今回はusersテーブルにレコードを追加
+$sql = "INSERT INTO users (name, password) VALUES (:name, :password)";
+// 関数db_connect()からPDOを取得する
+$pdo = db_connect();
+try {
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':password', $password);
+    $stmt->execute();
+    echo 'インサートしました。';
+} catch (PDOException $e) {
+    echo 'Error: ' . $e->getMessage();
+    die();
+}
---
+++ old3-2/index.php
@@ -0,0 +1,31 @@
+<!-- step1:フルーツと単価の連想配列を作成してください。 -->
+<?php
+
+$fruits = ["りんご" => 300, "みかん" => 150, "桃" => 3000];
+$fruits_array = [1, 1, 1];
+?>
+
+<!-- step2:単価を計算する関数を定義してください。 -->
+
+<?php
+$i = 0;
+
+function fruits_sale($fruits , $fruits_array){
+$sale = $fruits * $fruits_array ; 
+return $sale ;
+}
+
+?>
+
+<!-- step3:繰り返しを使ってそれぞれのフルーツを書き出してください。 -->
+
+<?php
+
+foreach ($fruits as $key => $value) {
+    echo $key."は".fruits_sale($value , $fruits_array[$i])."円です。";
+    $i++; 
+    echo '<br>';   
+   }
+
+
+?>
